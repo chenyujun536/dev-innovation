bdea45144995ca9441de289833cf6fc500840b14
Yujun CHEN <YChen32@slb.com>
2022-11-23 11:50:05 +0800

    screen capture every 30 seconds at background

---
+++ ScreenCapture/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UsageReport;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        private string imageFolder = "C:\\Temp";
+
+        public Form1()
+        {
+            InitializeComponent();
+            MakeDir();
+        }
+
+        private void MakeDir()
+        {
+            try
+            {
+                var path = Assembly.GetEntryAssembly()?.Location;
+                var directory = (path == null) ? "C:\\Temp" : Path.GetDirectoryName(path);
+
+                string subfolder = $"{DateTime.Now:yyyy-MM-dd-HH-mm-ss}";
+                imageFolder = Path.Combine(directory, subfolder);
+                Directory.CreateDirectory(imageFolder);
+            }
+            catch (Exception e)
+            {
+                Logger.Info(e.ToString());
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {   
+                int width = Screen.PrimaryScreen.Bounds.Width;
+                int height = Screen.PrimaryScreen.Bounds.Height;
+                //var size =Screen.PrimaryScreen.Bounds.Size;
+                Bitmap captureBitmap = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+                Rectangle captureRectangle = Screen.PrimaryScreen.Bounds;
+                Graphics captureGraphics = Graphics.FromImage(captureBitmap);
+                captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0,
+                    captureRectangle.Size);
+
+                captureBitmap.Save(Path.Combine(imageFolder, $"snapshot_{DateTime.Now:HH-mm-ss}.jpg"), ImageFormat.Jpeg);
+            }
+            catch(Exception ex)
+            {
+                Logger.Info(ex.ToString());
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            var internalString = ConfigurationManager.AppSettings["SnapshotInterval"];
+            if (Int32.TryParse(internalString, out int intervalNumber))
+            {
+                this.timer1.Interval = intervalNumber;
+            }
+        }
+    }
+}
---
+++ ScreenCapture/Logger.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+
+namespace UsageReport
+{
+    public static class Logger
+    {
+
+        static Logger()
+        {
+            string logFile;
+            try
+            {
+                var path = Assembly.GetEntryAssembly()?.Location;
+                var directory = Path.GetDirectoryName(path);
+                var fileName = Path.GetFileNameWithoutExtension(path);
+                logFile = Path.Combine(directory, $"{fileName}_log_{DateTime.Now:HH-mm-ss.fff}.txt");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                logFile = @"C:\\Temp\\sampletestlog.txt";
+            }
+
+            LogSw = new StreamWriter(logFile);
+        }
+
+        private static readonly StreamWriter LogSw; // = new StreamWriter(logFile);
+        private static readonly object LockObject = new object();
+
+        private static void LogLine(string line, bool debug)
+        {
+            lock (LockObject)
+            {
+                if (!debug)
+                    Console.WriteLine(
+                        $"[{DateTime.Now:HH-mm-ss.fff}] [Thread {Thread.CurrentThread.ManagedThreadId}] {line}");
+
+                if (LogSw != null)
+                {
+                    LogSw?.WriteLine(
+                        $"[{DateTime.Now:HH-mm-ss.fff}] [Thread {Thread.CurrentThread.ManagedThreadId}] {line}");
+                    LogSw?.Flush();
+                }
+            }
+        }
+
+        public static void Info(string line)
+        {
+            LogLine(line, false);
+        }
+
+        public static void Debug(string line)
+        {
+            LogLine(line, true);
+        }
+
+    }
+}
